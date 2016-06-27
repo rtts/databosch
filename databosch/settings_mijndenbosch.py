8efb8acdb186038b7c9904f832c51ea1ad113a39
Jaap Joris Vens <jj@rtts.eu>
2016-06-27 14:33:11 +0200

    Changed default from email

--- databosch/settings_mijndenbosch.py
+++ databosch/settings_mijndenbosch.py
@@ -2,5 +2,5 @@ from .settings_common import *
 
 ROOT_URLCONF = 'databosch.urls_mijndenbosch'
 WSGI_APPLICATION = 'databosch.wsgi_mijndenbosch.application'
-DEFAULT_FROM_EMAIL = 'noreply@mijndenbosch.nl'
+DEFAULT_FROM_EMAIL = 'info@mijndenbosch.nl'
 SITE_ID = 3
