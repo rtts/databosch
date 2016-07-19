f48d525ede3e7d37322232c21a0c889899c22060
Jaap Joris Vens <jj@rtts.eu>
2016-07-19 15:01:27 +0200

    Added toolkit files and custom URL for them

--- databosch/settings_common.py
+++ databosch/settings_common.py
@@ -75,6 +75,7 @@ INSTALLED_APPS = [
 if DEBUG:
     INSTALLED_APPS += [
         'debug_toolbar',
+        'django_extensions',
     ]
 
 MIDDLEWARE_CLASSES = [
--- mijndenbosch/urls.py
+++ mijndenbosch/urls.py
@@ -1,6 +1,14 @@
+from django.conf import settings
 from django.conf.urls import url, include
+from django.http import HttpResponse
 from .forms import *
 from .views import *
+
+def toolkit(request):
+    response = HttpResponse()
+    response['Content-Disposition'] = "attachment; filename=toolkit.zip"
+    response['X-Accel-Redirect'] = "{}toolkit.zip".format(settings.STATIC_URL)
+    return response
 
 urlpatterns = [
     url(r'^accounts/register/$', Aanmelden.as_view(), name='registration_register'),
@@ -15,5 +23,6 @@ urlpatterns = [
     url(r'^initiatieven/$', initiatieven, name='initiatieven'),
     url(r'^nieuws/$', news, name='news'),
     url(r'^bijeenkomst/([0-9]+)/$', bijeenkomst, name='bijeenkomst'),
+    url(r'^toolkit/$', toolkit),
     url(r'^([^/]+)/$', netwerk, name='netwerk'),
 ]
