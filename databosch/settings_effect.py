141914ce4c66505d2102ba5f517634f584365f8b
Jaap Joris Vens <jj@rtts.eu>
2017-08-21 21:38:15 +0200

    Automated commit for deployment

--- databosch/settings_effect.py
+++ databosch/settings_effect.py
@@ -5,7 +5,7 @@ from .settings_common import *
 ROOT_URLCONF = 'effect.urls'
 WSGI_APPLICATION = 'databosch.wsgi_effect.application'
 SITE_ID = 7
-
+ALLOWED_HOSTS += ['effect.created.today']
 MIDDLEWARE += ['tidy.middleware.TidyMiddleware']
 INSTALLED_APPS += ['sass_processor']
 SASS_PROCESSOR_ROOT = os.path.join(os.path.dirname(os.path.abspath(effect.__file__)), 'static')
