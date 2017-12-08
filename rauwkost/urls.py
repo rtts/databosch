d558a22638909bb462a277934f921e0bf6186d73
Jaap Joris Vens <jj@rtts.eu>
2017-12-08 15:48:36 +0100

    Automated commit for deployment

--- rauwkost/urls.py
+++ rauwkost/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import RedirectView
 from .views import *
 
 urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
-    url(r'^$', RedirectView.as_view(pattern_name='location')),
+    url(r'^$', RedirectView.as_view(pattern_name='locations')),
     url(r'^locaties/$', ProgramLocationView.as_view(), name='locations'),
     url(r'^locaties/(?P<slug>[^/]+)/$', ProgramLocationView.as_view(), name='location'),
     url(r'^tijden/$', ProgramTimeView.as_view(), name='times'),
