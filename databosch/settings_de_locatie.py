9e3a07f8c7064ac456a2fb361b436b04e710864b
Jaap Joris Vens <jj@rtts.eu>
2018-01-18 14:45:56 +0100

    Automated commit for deployment

--- databosch/settings_de_locatie.py
+++ databosch/settings_de_locatie.py
@@ -1,5 +1,6 @@
 from .settings_common import *
 
 ROOT_URLCONF = 'de_locatie.urls'
+ALLOWED_HOSTS += ['locatie.created.today']
 WSGI_APPLICATION = 'databosch.wsgi_de_locatie.application'
 SITE_ID = 6
