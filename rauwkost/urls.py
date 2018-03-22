7da9614073197f55cd99166f6373af52e5e403b8
Jaap Joris Vens <jj@rtts.eu>
2018-03-22 17:39:13 +0100

    Automated commit for deployment

--- rauwkost/urls.py
+++ rauwkost/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
-from .views import *
 from .views2018 import *
 
 urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
