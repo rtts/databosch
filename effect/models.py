409d00b8c2afa4df8473c403f124fa9ddd6ec486
Jaap Joris Vens <jj@rtts.eu>
2018-06-06 11:16:38 +0200

    Automated commit for deployment

--- effect/models.py
+++ effect/models.py
@@ -137,6 +137,9 @@ class News(models.Model):
     content = RichTextField('inhoud', blank=True)
     project = models.ForeignKey('Project', blank=True, null=True, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
+
     class Meta:
         ordering = ['-date']
         verbose_name = 'Nieuwsbericht'
