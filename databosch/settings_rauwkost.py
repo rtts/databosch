ece75ac6518913d28a29fa6a92aeeb2a44f5f870
Jaap Joris Vens <jj@rtts.eu>
2019-11-19 22:00:57 +0100

    remove unused dependencies

--- databosch/settings_rauwkost.py
+++ databosch/settings_rauwkost.py
@@ -6,6 +6,5 @@ ROOT_URLCONF = 'rauwkost.urls'
 WSGI_APPLICATION = 'databosch.wsgi_rauwkost.application'
 SITE_ID = 8
 ALLOWED_HOSTS += ['www.rauwkost.online']
-#MIDDLEWARE += ['tidy.middleware.TidyMiddleware']
 INSTALLED_APPS += ['sass_processor']
 SASS_PROCESSOR_ROOT = os.path.join(os.path.dirname(os.path.abspath(rauwkost.__file__)), 'static')
